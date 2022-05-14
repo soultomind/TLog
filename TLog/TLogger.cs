f5d4f7df4ffc14159fca645e46e77eea436a87f3
soultomind <soultomind@naver.com>
2022-05-15 02:10:02 +0900

    Feat : 프로젝트 생성

---
+++ TLog/TLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraceLog
+{
+    /// <summary>
+    /// <see cref="System.Diagnostics.Trace"/> 로그 클래스입니다.
+    /// </summary>
+    public class TLogger
+    {
+        public static string PatternLayout = "%c:%m";
+        private static string MakeHeader()
+        {
+            string className = new StackFrame(2).GetMethod().ReflectedType.Name;
+            string methodName = new StackFrame(2, true).GetMethod().Name;
+            return String.Format("{0}:{1}", className, methodName);
+        }
+        public static void Write(string message)
+        {
+            string header = MakeHeader();
+
+            message = String.Format("{0} {1}", header, message);
+            Trace.Write(message);
+        }
+
+        public static void WriteLine(string message)
+        {
+            string header = MakeHeader();
+
+            message = String.Format("{0} {1}", header, message);
+            Trace.WriteLine(message);
+        }
+    }
+}
