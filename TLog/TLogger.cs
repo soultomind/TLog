b2a3f657e2a934ed17bdf5a5baaa521bb40bd91f
soultomind <soultomind930@gmail.com>
2022-05-16 23:33:23 +0900

    Test. Git AccessToken 테스트

--- TLog/TLogger.cs
+++ TLog/TLogger.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TraceLog
+namespace TLog
 {
     /// <summary>
     /// <see cref="System.Diagnostics.Trace"/> 로그 클래스입니다.
@@ -13,17 +13,26 @@ namespace TraceLog
     public class TLogger
     {
         public static string PatternLayout = "%c:%m";
+
+        private static string MethodReflectedTypeName
+        {
+            get { return new StackFrame(2).GetMethod().ReflectedType.Name; }
+        }
+
+        private static string MethodName
+        {
+            get { return new StackFrame(2).GetMethod().Name; }
+        }
+
         private static string MakeHeader()
         {
-            string className = new StackFrame(2).GetMethod().ReflectedType.Name;
-            string methodName = new StackFrame(2, true).GetMethod().Name;
-            return String.Format("{0}:{1}", className, methodName);
+            return String.Format("{0}:{1}", MethodReflectedTypeName, MethodName);
         }
         public static void Write(string message)
         {
             string header = MakeHeader();
 
-            message = String.Format("{0} {1}", header, message);
+            message = String.Format("{0}{1}", header, message);
             Trace.Write(message);
         }
 
@@ -31,7 +40,7 @@ namespace TraceLog
         {
             string header = MakeHeader();
 
-            message = String.Format("{0} {1}", header, message);
+            message = String.Format("{0}{1}", header, message);
             Trace.WriteLine(message);
         }
     }
