cc7ea413b9461763b13c0253a763a8aa97c11a32
soultomind <soultomind930@gmail.com>
2022-05-17 22:02:53 +0900

    Feat. 단위 테스트 프로젝트 추가

---
+++ TLog.UnitTest/LayoutTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TLog.UnitTest
+{
+    [TestClass]
+    public class LayoutTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
