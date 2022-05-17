11508689e4025916caa12e71316821827e23ccf2
soultomind <soultomind930@gmail.com>
2022-05-17 22:02:29 +0900

    Feat. 테스트 프로젝트 추가

---
+++ TLog.TestProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TLogger.WriteLine("출력");
+
+            string layout = TLogger.ConsolePatternLayout;
+            int indexOf = TLogger.Class.IndexOf(layout);
+            TLogger.WriteLine("Method.IndexOf=" + indexOf);
+            indexOf = TLogger.Message.IndexOf(layout);
+            TLogger.WriteLine("Method.IndexOf=" + indexOf);
+            Console.ReadLine();
+        }
+    }
+}
