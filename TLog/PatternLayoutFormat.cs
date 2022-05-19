41fa9fabc414e9715b184e0f91cdeb1da86a8fb0
soultomind <soultomind930@gmail.com>
2022-05-19 19:33:18 +0900

    Refactor: 내부 Private 함수 PatternLayoutFormat 으로 이동 및 패턴 포맷값 ThreadSafe 처리

--- TLog/PatternLayoutFormat.cs
+++ TLog/PatternLayoutFormat.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TLog
 {
-    class PatternLayoutFormat
+    public class PatternLayoutFormat
     {
+        public string UseCurrentPatternFormat { get; set; } = String.Empty;
+        public List<PatternLayoutTypeComparable> TypeOrders
+        {
+            get;
+            private set;
+        }
+
+        private static readonly object _Lock = new object();
+        public PatternLayoutFormat()
+        {
+            TypeOrders = new List<PatternLayoutTypeComparable>();
+        }
+
+        public void Process(IPatternLayoutType[] layoutTypes, string patternLayoutFormat)
+        {
+            List<PatternLayoutTypeComparable> list = new List<PatternLayoutTypeComparable>();
+
+            int indexOf = -1;
+            foreach (PatternLayoutType layoutType in layoutTypes)
+            {
+                indexOf = layoutType.IndexOf(patternLayoutFormat);
+                if (indexOf != -1)
+                {
+                    list.Add(new PatternLayoutTypeComparable(indexOf, layoutType));
+                }
+            }
+
+            list.Sort();
+            TypeOrders = list;
+        }
+
+        public string CreateFormatMessage(string patternLayoutFormat, string[] args)
+        {
+            if (UseCurrentPatternFormat == String.Empty)
+            {
+                lock (_Lock)
+                {
+                    if (UseCurrentPatternFormat == String.Empty)
+                    {
+                        Trace.WriteLine("Set UseCurrentPatternFormat!");
+                        string format = patternLayoutFormat;
+                        for (int i = 0; i < TypeOrders.Count; i++)
+                        {
+                            IPatternLayoutType layoutType = TypeOrders[i].LayoutType;
+                            format = format.Replace(layoutType.LayoutTypeString, "{" + i + "}");
+                        }
+
+                        UseCurrentPatternFormat = format;
+                    }
+                }                
+            }
+            return String.Format(UseCurrentPatternFormat, args);
+        }
     }
 }
--- TLog/TLogger.cs
+++ TLog/TLogger.cs
@@ -13,60 +13,64 @@ namespace TLog
     /// </summary>
     public class TLogger
     {
+        public static bool IsConfigure { get; private set; }
+
         /// <summary>
-        /// 클래스명
+        /// 클래스명 패턴레이아웃
         /// </summary>
         public static readonly IPatternLayoutType Class = new ClassPatternLayout("C");
 
         /// <summary>
-        /// 현재날짜
+        /// 현재날짜 패턴레이아웃
         /// </summary>
-        public static readonly IPatternLayoutType Date = new DatePatternLayout("date");
+        public static readonly IPatternLayoutType Date = new DatePatternLayout("date") { Format = "yyyy-MM-dd hh:mm:ss:ffff" };
 
         /// <summary>
-        /// DebugView Filter Include Filter 값
+        /// DebugView Filter Include Filter 값 패턴레이아웃
         /// </summary>
         public static readonly IPatternLayoutType IncludeFilter = new IncludeFilterPatternLayout("includefilter");
 
         /// <summary>
-        /// 로그레벨
+        /// 로그레벨 패턴레이아웃
         /// </summary>
         public static readonly IPatternLayoutType Level = new LevelPatternLayout("level");
 
         /// <summary>
-        /// 메소드명
+        /// 메소드명 패턴레이아웃
         /// </summary>
         public static readonly IPatternLayoutType Method = new MethodPatternLayout("M");
 
         /// <summary>
-        /// 메시지
+        /// 메시지 패턴레이아웃
         /// </summary>
         public static readonly IPatternLayoutType Message = new MessagePatternLayout("message");
 
         /// <summary>
-        /// 개행
+        /// 개행 패턴레이아웃
         /// </summary>
         public static readonly IPatternLayoutType NewLine = new NewLinePatternLayout("newline");
 
         /// <summary>
-        /// 현재스레드
+        /// 현재스레드 패턴레이아웃
         /// </summary>
         public static readonly IPatternLayoutType Thread = new ThreadPatternLayout("thread");
 
-        private static readonly IPatternLayoutType[] LayoutTypeStrings = new IPatternLayoutType[] 
+        /// <summary>
+        /// 패턴레이아웃 배열
+        /// </summary>
+        public static readonly IPatternLayoutType[] LayoutTypes = new IPatternLayoutType[] 
         {
             Class, Date, IncludeFilter, Level, Method, Message, NewLine, Thread
-
         };
+
         /// <summary>
-        /// <see cref="System.Diagnostics.ConsoleTraceListener"/>에 출력하는 패턴 레이아웃입니다.
+        /// <see cref="System.Diagnostics.DefaultTraceListener"/>에 출력하는 패턴 레이아웃입니다.
         /// </summary>
         /// <remarks>
         /// 
         /// </remarks>
         public static string DefaultPatternLayout = "%level %includefilter %C:%M %date %message";
-        private static List<PatternLayoutTypeComparable> DefaultPatternLayoutTypes;
-        private static string DefaultPatternFormat = String.Empty;
+        private static PatternLayoutFormat LayoutFormat = new PatternLayoutFormat();
 
 
         /// <summary>
@@ -79,15 +83,9 @@ namespace TLog
         /// </summary>
         public static string EventLogLayout = DefaultPatternLayout;
 
-        /// <summary>
-        /// %date 패턴 사용시 사용되는 포맷입니다.
-        /// </summary>
-        public static string DateTimeFormat = "yyyy-MM-dd hh:mm:ss:ffff";
-
         #region Private
         private static bool HasStackFrameLayout(string typeName)
         {
-            // TODO: typeof(DefaultTraceListener) 시에 Type.Name 값이 RuntimeType 조사필요
             foreach (TraceListener itemListener in Trace.Listeners)
             {
                 Type type = itemListener.GetType();
@@ -116,26 +114,17 @@ namespace TLog
             }
             return false;
         }
-
-        private static string CreateFormatText(string[] args)
-        {
-            if (DefaultPatternFormat == String.Empty)
-            {
-                // "%level %includefilter %C:%M %date %message";
-                string format = DefaultPatternLayout;
-                for (int i = 0; i < DefaultPatternLayoutTypes.Count; i++)
-                {
-                    IPatternLayoutType layoutType = DefaultPatternLayoutTypes[i].LayoutType;
-                    format = format.Replace(layoutType.LayoutTypeString, "{" + i + "}");
-                }
-
-                DefaultPatternFormat = format;
-                return String.Format(format, args);
-            }
-
-            return String.Format(DefaultPatternFormat, args);
-        }
+        
         #endregion
+
+        /// <summary>
+        /// %date 패턴 사용시 사용되는 포맷입니다.
+        /// </summary>
+        public static string DateTimeFormat
+        {
+            get { return (Date as DatePatternLayout).Format; }
+            set { (Date as DatePatternLayout).Format = value; }
+        }
 
         /// <summary>
         /// 디버그뷰에서 사용되는 Filter/Include Filter 에 사용되는 속성입니다.
@@ -148,67 +137,48 @@ namespace TLog
 
         public static void Configure()
         {
-            List<string> format = new List<string>();
-            StringBuilder builder = new StringBuilder();
-
-            int indexOf = -1;
-
-            // TOOD: 아래 패턴레이아웃에서 사용되는 모든 타입에 대해서 위치 값 구한후 Format 설정필요.
-            if (HasStackFrameLayout("DefaultTraceListener"))
+            if (!IsConfigure)
             {
-                List<PatternLayoutTypeComparable> list = new List<PatternLayoutTypeComparable>();
-                foreach (PatternLayoutType layoutType in LayoutTypeStrings)
+                // TODO: typeof(DefaultTraceListener) 시에 Type.Name 값이 RuntimeType 조사필요
+                if (HasStackFrameLayout("DefaultTraceListener"))
                 {
-                    indexOf = layoutType.IndexOf(DefaultPatternLayout);
-                    if (indexOf != -1)
-                    {
-                        list.Add(new PatternLayoutTypeComparable(indexOf, layoutType));
-                    }
+                    LayoutFormat.Process(LayoutTypes, DefaultPatternLayout);
                 }
 
-                list.Sort();
-                DefaultPatternLayoutTypes = list;
-                
-                // 다시 루프 돌면서 
-            }
+                #region TODO TextWriter, EventLog 작업
 
-            #region TODO TextWriter, EventLog 작업
-            
-            //TextWriterTraceListener
-            //EventLogTraceListener 
-            
-            #endregion
+                //TextWriterTraceListener
+                //EventLogTraceListener 
+
+                #endregion
+
+                IsConfigure = true;
+            }
         }
 
         private static string MakeMessage(LogLevel logLevel, string message)
         {
-            // TOOD: Debug 로그레벨이 현재 사용가능한지 체크 후 출력 처리
+            // TOOD: 로그레벨이 현재 사용가능한지 체크 후 출력 처리
 
-            // "%level %a %C:%M %date %message";
-
-            string[] args = new string[DefaultPatternLayoutTypes.Count];
+            string[] args = new string[LayoutFormat.TypeOrders.Count];
             StackFrame sf = null;
-            if (HasReflectPatternLayout(DefaultPatternLayoutTypes))
+            if (HasReflectPatternLayout(LayoutFormat.TypeOrders))
             {
                 sf = new StackFrame(2, true);
             }
 
             string arg = String.Empty;
-            for (int i = 0; i < DefaultPatternLayoutTypes.Count; i++)
+            for (int i = 0; i < LayoutFormat.TypeOrders.Count; i++)
             {
                 // Class, Date, IncludeFilter, Level, Method, Message, NewLine, Thread
-                IPatternLayoutType layoutType = DefaultPatternLayoutTypes[i].LayoutType;
+                IPatternLayoutType layoutType = LayoutFormat.TypeOrders[i].LayoutType;
                 if (IsReflectPatternLayout(layoutType))
                 {
                     arg = layoutType.ConvertArgument(sf);
                 }
-                else if (layoutType.GetType() == Date.GetType())
-                {
-                    arg = layoutType.ConvertArgument(DateTimeFormat);
-                }
                 else if (layoutType.GetType() == Level.GetType())
                 {
-                    arg = layoutType.ConvertArgument(LogLevel.Debug);
+                    arg = layoutType.ConvertArgument(logLevel);
                 }
                 else if (layoutType.GetType() == Message.GetType())
                 {
@@ -221,8 +191,20 @@ namespace TLog
                 args[i] = arg;
             }
 
-            message = CreateFormatText(args);
+            message = LayoutFormat.CreateFormatMessage(DefaultPatternLayout, args);
             return message;
+        }
+
+        public static void TraceWrite(string message)
+        {
+            message = MakeMessage(LogLevel.Trace, message);
+            Trace.Write(message);
+        }
+
+        public static void TraceWriteLine(string message)
+        {
+            message = MakeMessage(LogLevel.Trace, message);
+            Trace.WriteLine(message);
         }
 
         public static void DebugWrite(string message)
@@ -237,21 +219,27 @@ namespace TLog
             Trace.WriteLine(message);
         }
 
-        public static void Write(string message)
+        public static void InfoWrite(string message)
         {
-            StackFrame sf = new StackFrame(1, true);
-            string header = String.Format("{0}:{1}", sf.GetMethod().ReflectedType.Name, sf.GetMethod().Name);
-
-            message = String.Format("{0} {1} {2}", header, DateTime.Now.ToString(DateTimeFormat), message);
+            message = MakeMessage(LogLevel.Info, message);
             Trace.Write(message);
         }
 
-        public static void WriteLine(string message)
+        public static void InfoWriteLine(string message)
         {
-            StackFrame sf = new StackFrame(1, true);
-            string header = String.Format("{0}:{1}", sf.GetMethod().ReflectedType.Name, sf.GetMethod().Name);
+            message = MakeMessage(LogLevel.Info, message);
+            Trace.WriteLine(message);
+        }
 
-            message = String.Format("{0} {1} {2}", header, DateTime.Now.ToString(DateTimeFormat), message);
+        public static void WarnWrite(string message)
+        {
+            message = MakeMessage(LogLevel.Warn, message);
+            Trace.Write(message);
+        }
+
+        public static void WarnWriteLine(string message)
+        {
+            message = MakeMessage(LogLevel.Warn, message);
             Trace.WriteLine(message);
         }
     }
